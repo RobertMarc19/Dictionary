b6dd3d769a04f2b87e6d7444f428d07e1936a8e3
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-11-19 12:02:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let arrayOfWords = new Array();
+let wordIndex = 0;
+function saveWord(event) {
+  event.preventDefault();
+  let found = 0;
+  let currentWord = document.getElementById("textInput").value;
+  for (let i = 0; i < wordIndex; ++i) {
+    if (currentWord == arrayOfWords[i]) {
+      found = 1;
+      break;
+    }
+  }
+  if (found == 0) {
+    arrayOfWords[wordIndex++] = currentWord;
+  }
+}
+
+function searchWord(event) {
+  let searchedWord = document.getElementById("textInput").value;
+  event.preventDefault();
+  for (let i = 0; i < wordIndex; ++i) {
+    if (searchedWord == arrayOfWords[i]) {
+      alert("This word exists in dictionary");
+      break;
+    } else {
+      alert("This word doesn't exist in dictionary");
+    }
+  }
+}
